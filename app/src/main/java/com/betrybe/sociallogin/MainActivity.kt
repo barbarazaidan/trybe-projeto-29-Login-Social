e7067e9b24c18ddf59f31c9ffea0285254b83926
Bárbara <babivaldez@gmail.com>
2024-02-06 20:29:01 -0300

    testa req07 de novo

--- app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
+++ app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
@@ -7,6 +7,7 @@ import android.text.TextWatcher
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 
 class MainActivity : AppCompatActivity() {
@@ -15,8 +16,8 @@ class MainActivity : AppCompatActivity() {
 
     // crio as variáveis, digo que vou dar o valor delas depois com o lateinit e defino o tipo
     private lateinit var buttonLogin: Button
-    private lateinit var inputEmail: TextInputEditText
-    private lateinit var password: TextInputEditText
+    private lateinit var inputEmail: TextInputLayout
+    private lateinit var password: TextInputLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,30 +28,16 @@ class MainActivity : AppCompatActivity() {
         inputEmail = findViewById(R.id.email_text_input_layout)
         password = findViewById(R.id.password_text_input_layout)
 
-
-        inputEmail.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(p0: Editable?) {
-                enableButtonLogin()
-            }
-
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
-
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
-        })
-
-        password.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(p0: Editable?) {
-                enableButtonLogin()
-            }
-
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
-
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
-        })
+        buttonLogin.setOnClickListener {
+            enableButtonLogin()
+        }
     }
-    fun enableButtonLogin() {
 
-        if (!TextUtils.isEmpty(password.toString()) && !TextUtils.isEmpty(inputEmail.toString())) {
+    fun enableButtonLogin() {
+        var emailText = inputEmail.editText.toString()
+        var passwordText = password.editText.toString()
+
+        if (!TextUtils.isEmpty(passwordText) && !TextUtils.isEmpty(emailText)) {
             buttonLogin.isEnabled
         }
     }
