8f45794714e30c83cae3460d47b60f86cdcfaba2
Bárbara <babivaldez@gmail.com>
2024-02-07 11:54:31 -0300

    finaliza req09

--- app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
+++ app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
@@ -2,12 +2,14 @@ package com.betrybe.sociallogin
 
 import android.os.Bundle
 import android.text.Editable
-import android.text.TextUtils
 import android.text.TextWatcher
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
+import java.util.regex.Pattern
+
 
 
 class MainActivity : AppCompatActivity() {
@@ -15,11 +17,14 @@ class MainActivity : AppCompatActivity() {
     // iniciando projeto
 
     // crio as variáveis, digo que vou dar o valor delas depois com o lateinit e defino o tipo
+    // usei como referência para desenvolver a validação do botão de login o vídeo https://www.youtube.com/watch?v=Frv-6bDslxE
     private lateinit var buttonLogin: Button
     private lateinit var textPassword: TextInputEditText
+    private lateinit var passwordInput: TextInputLayout
     private lateinit var textEmail: TextInputEditText
     private var isEmptyEmail = true
     private var isEmptyPassword = true
+    private val emailRegex = "/S+@+w+.+[c]+[0]+[m]/"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +33,7 @@ class MainActivity : AppCompatActivity() {
         // atribuo valor às variáveis. Preciso fazer isso dentro do escopo desta função onCreate
         buttonLogin = findViewById(R.id.login_button)
         textPassword = findViewById(R.id.password_text)
+        passwordInput = findViewById(R.id.password_text_input_layout)
         textEmail = findViewById(R.id.email_text)
 
         textPassword.addTextChangedListener(object : TextWatcher {
@@ -54,6 +60,16 @@ class MainActivity : AppCompatActivity() {
                 enableButtonLogin()
             }
         })
+
+        buttonLogin.setOnClickListener {
+            val passwordSize = textPassword.text.toString().length
+            print(passwordSize)
+            if (passwordSize <= 4) {
+                passwordInput.error = "Senha deve ter mais de 4 caracteres"
+            } else {
+                passwordInput.error = null
+            }
+        }
     }
     private fun enableButtonLogin() {
         buttonLogin.isEnabled = !isEmptyEmail && !isEmptyPassword
